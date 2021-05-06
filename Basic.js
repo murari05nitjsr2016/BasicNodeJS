820bab013df814b386166ee07019c64617bc0a89
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-07 04:11:59 +0530

    added:Max Among 5 numbers

--- Basic.js
+++ Basic.js
@@ -1,3 +1,28 @@
+
+
 {
-    console.log("Welcome in Node js");
+    let num1 = Math.floor(Math.random()*(999-100+1)+100);
+    let num2 = Math.floor(Math.random()*(999-100+1)+100);
+    let num3 = Math.floor(Math.random()*(999-100+1)+100);
+    let num4 = Math.floor(Math.random()*(999-100+1)+100);
+    let num5 = Math.floor(Math.random()*(999-100+1)+100);
+    let max = 0;
+    let min = 0;
+    if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5)
+    {
+        max = num1;
+        console.log(max);
+    }else if(num2 > num3 && num2 > num4 && num2 > num5){
+        max = num2;
+        console.log(max);
+    }else if(num3 > num4 && num3 > num5){
+        max = num3;
+        console.log(max);
+    }else if(num4 > num5){
+        max = num4;
+        console.log(max);
+    }else{
+        max = num5;
+        console.log(max);
+    }
 }
