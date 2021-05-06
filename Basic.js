d01c645bd424bfb6f853425f2c38b04e27c60bb2
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-07 04:20:50 +0530

    added:Minimum among five numbers

--- Basic.js
+++ Basic.js
@@ -25,4 +25,24 @@
         max = num5;
         console.log(max);
     }
+
+    if(num1 < num2 && num1 < num3 && num1 < num4 && num1 < num5)
+    {
+        min = num1;
+        console.log(min);
+    }else if(num2 < num3 && num2 < num4 && num2 < num5){
+        min = num2;
+        console.log(min);
+    }else if(num3 < num4 && num3 < num5){
+        min = num3;
+        console.log(min);
+    }else if(num4 < num5){
+        min = num4;
+        console.log(min);
+    }else{
+        min = num5;
+        console.log(min);
+    }
+
 }
+
