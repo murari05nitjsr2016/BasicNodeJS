5716f4886243aba2f0d7c0bef72abdd3f7bc0249
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-07 08:14:08 +0530

    added:Convert digit to Word

--- Basic.js
+++ Basic.js
@@ -66,6 +66,34 @@ function flipCoin(){
     }
 }
 
-flipCoin();
+//flipCoin();
 
-
+function digitToWord(){
+    const prompt = require('prompt-sync')({sigint: true});  
+    let digit = prompt('Enter the digit :');
+    digit = Number(digit); 
+    if(digit == 0)
+    { 
+    console.log("ZERO");
+    }else if(digit == 1)
+    {
+        console.log("ONE");  
+    }else if(digit == 2){
+        console.log("TWo");
+    }else if(digit == 3){
+        console.log("THREE");
+    }else if(digit == 4){
+        console.log("FOUR");
+    }else if(digit == 5){
+        console.log("FIVE");
+    }else if(digit == 6){
+        console.log("SIX");
+    }else if(digit == 7){
+        console.log("SEVEN");
+    }else if(digit == 8){
+        console.log("EIGHT");
+    }else{
+        console.log("NINE");
+    }
+}
+digitToWord();
